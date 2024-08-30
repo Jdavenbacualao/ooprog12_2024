f09cbf463371de47656ee5dafaa1e533cfeac0e8
Jdavenbacualao <bdavendaniel@gmail.com>
2024-08-30 08:04:37 +0700

    Update warmup_sleepin.java

--- Chapter 1/warmup_sleepin.java
+++ Chapter 1/warmup_sleepin.java
@@ -1,13 +1,13 @@
-public class SleepIn {
+ public class SleepIn {
 
     public static boolean sleepIn(boolean weekday, boolean vacation) {
         return !weekday || vacation;
     }
 
     public static void main(String[] args) {
-        // Test cases
-        System.out.println(sleepIn(false, false)); // true
-        System.out.println(sleepIn(true, false));  // false
-        System.out.println(sleepIn(false, true));  // true
+        SleepIn sleep = new SleepIn();
+        System.out.println("Vacation(false, false) =" + sleep.sleepIn(false,false));
+        System.out.println("Vacation(true, false) =" + sleep.sleepIn(true,false));
+        System.out.println("Vacation(false, true) =" + sleep.sleepIn(false,true));
     }
 }
