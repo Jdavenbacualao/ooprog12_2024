4e362078cf72901b3152d88860b9c234b4d7417d
Jdavenbacualao <bdavendaniel@gmail.com>
2024-09-20 21:26:09 +0800

    Create FirstAddressExcercise.java

---
+++ Chapter 3/FirstAddressExcercise.java
@@ -0,0 +1,17 @@
+public class Company{
+    public static void main(String[] a){
+        
+        String companyName =" Realme Mobile Telecommunications (Shenzhen) Co., Ltd. ";
+         String addressLine ="Room201,Shenzhen, China";
+          String citystateZip =" Shenzhen Guangdong  518000.";
+           String parentCompany = " Oppo (Subsidiary under BBK Electronics)"; 
+
+           System.out.println("Company information:");
+            System.out.println("===================");
+             System.out.println("Name: " + companyName);
+              System.out.println("Address: " + addressLine);
+               System.out.println("City, State, Zip: " + citystateZip);
+                System.out.println("Associated Company: " + parentCompany);
+            
+    }
+} 
